88367f10a81f0b7779a6bace510c919d9975f721
krittt <krit.nsit@gmail.com>
2018-03-29 20:19:22 -0500

    props

---
+++ Prop_types/client/main.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Meteor} from 'meteor/meteor';
+import {Tracker} from 'meteor/tracker';
+
+import {Players} from './../imports/api/players';
+import TitleBar from './../imports/ui/TitleBar';
+import AddPlayer from './../imports/ui/AddPlayer';
+
+const renderPlayers = (playersList) => {
+  return playersList.map((player) => {
+    return (
+      <p key={player._id}>
+        {player.name} has {player.score} point(s).
+        <button onClick={() => {
+          Players.update(player._id, {$inc: {score: -1}});
+        }}>-1</button>
+        <button onClick={() => {
+          Players.update(player._id, {$inc: {score: 1}});
+        }}>+1</button>
+        <button onClick={() => Players.remove(player._id)}>X</button>
+      </p>
+    );
+  });
+};
+
+const handleSubmit = (e) => {
+  let playerName = e.target.playerName.value;
+
+  e.preventDefault();
+
+  if (playerName) {
+    e.target.playerName.value = '';
+    Players.insert({
+      name: playerName,
+      score: 0
+    });
+  }
+};
+
+Meteor.startup(() => {
+  Tracker.autorun(() => {
+    let players = Players.find().fetch();
+    let title = 'Score Keep';
+    let jsx = (
+      <div>
+        <TitleBar title={title} subtitle="Created by Andrew Mead"/>
+        {renderPlayers(players)}
+        <AddPlayer/>
+        <form onSubmit={handleSubmit}>
+          <input type="text" name="playerName" placeholder="Player name"/>
+          <button>Add Player</button>
+        </form>
+      </div>
+    );
+    ReactDOM.render(jsx, document.getElementById('app'));
+  });
+});
